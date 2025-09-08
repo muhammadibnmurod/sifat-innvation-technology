8af9dc1a730cecb8381e28598013e35dc7302b87
Tohirov Muhammadsodiq <220481@centralasian.uz>
2025-09-08 16:09:31 +0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from './components/Header.jsx'
 import Navbar from './components/Navbar.jsx'
 import Home from './pages/Home.jsx'
+import Remont from "./components/Remont.jsx";
+import Partners from "./components/Partners.jsx";
 function App() {
   return (
     <BrowserRouter>
         <Header />
         <Navbar />
+        <Remont />
+        <Partners />
       <Routes>
         <Route path="/home" element={<Home />} />
       </Routes>
